bc08593f346d5ffd6181af78832c44aac72d4b1e
getrealweekend <getrealweekend@gmail.com>
2026-09-19 00:01:01 +0200

    Set GA4 Measurement ID for GET REAL Belgrade

--- analytics-config.js
+++ analytics-config.js
@@ -1,2 +1,2 @@
-// Add your GA4 Measurement ID after creating the Google Analytics property.
-window.GET_REAL_GA_ID = "";
+// GA4 Measurement ID for GET REAL Belgrade.
+window.GET_REAL_GA_ID = "G-7ZPFL1VFZ2";
